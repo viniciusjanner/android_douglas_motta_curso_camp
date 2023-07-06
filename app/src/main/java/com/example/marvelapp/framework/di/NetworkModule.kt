00b7e6de135670dad77adc1f660375e02120bd42
Vinicius Janner <viniciusjanner@gmail.com>
2023-07-06 20:53:21 -0300

    Implements repository in framework layer.

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package com.example.marvelapp.framework.di
 
 import com.example.marvelapp.BuildConfig
+import com.example.marvelapp.framework.network.MarvelApi
 import com.example.marvelapp.framework.network.interceptor.AuthorizationInterceptor
 import dagger.Module
 import dagger.Provides
@@ -60,10 +61,11 @@ object NetworkModule {
     fun provideRetrofit(
         okHttpClient: OkHttpClient,
         converterFactory: GsonConverterFactory,
-    ): Retrofit =
+    ): MarvelApi =
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(converterFactory)
             .build()
+            .create(MarvelApi::class.java)
 }
--- app/src/main/java/com/example/marvelapp/framework/network/MarvelApi.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/MarvelApi.kt
@@ -4,7 +4,7 @@ import com.example.marvelapp.framework.network.response.DataWrapperResponse
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
-interface MarvelAPI {
+interface MarvelApi {
 
     @GET("characters")
     suspend fun getCharacters(
