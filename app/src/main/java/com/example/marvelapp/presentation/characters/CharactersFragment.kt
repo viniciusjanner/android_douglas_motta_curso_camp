2c004ee98d4337e1647e79e389866b3b56efc395
Vinicius Janner <viniciusjanner@gmail.com>
2023-08-10 02:16:34 -0300

    Adding transition effect when going back to character screen.

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -84,6 +84,7 @@ class CharactersFragment : Fragment() {
     }
 
     private fun initCharactersAdapter() {
+        postponeEnterTransition()
         with(binding.recyclerCharacters) {
             setHasFixedSize(true)
             adapter = charactersAdapter.withLoadStateFooter(
@@ -94,6 +95,10 @@ class CharactersFragment : Fragment() {
                     charactersAdapter::retry,
                 ),
             )
+            viewTreeObserver.addOnPreDrawListener {
+                startPostponedEnterTransition()
+                true
+            }
         }
     }
 
