fb12d8641e767d26c2bb16ec82c8ab4e51a67b4c
Vinicius Janner <viniciusjanner@gmail.com>
2023-08-16 20:32:25 -0300

    Add descriptive comments to codes.

--- app/src/main/java/com/example/marvelapp/framework/db/dao/CharacterDao.kt
+++ app/src/main/java/com/example/marvelapp/framework/db/dao/CharacterDao.kt
@@ -14,6 +14,11 @@ interface CharacterDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(characters: List<CharacterEntity>)
 
+    //
+    // Para utilizarmos PagingSource e garantir o correto funcionamento do Room com o Paging
+    // temos que adicionar a dependencia
+    // implementation "androidx.room:room-paging:$room_version"
+    //
     @Query("SELECT * FROM ${DbConstants.CHARACTERS_TABLE_NAME}")
     fun pagingSource(): PagingSource<Int, CharacterEntity>
 
--- app/src/main/java/com/example/marvelapp/framework/db/entity/CharacterEntity.kt
+++ app/src/main/java/com/example/marvelapp/framework/db/entity/CharacterEntity.kt
@@ -7,6 +7,12 @@ import com.example.core.data.DbConstants
 
 @Entity(tableName = DbConstants.CHARACTERS_TABLE_NAME)
 data class CharacterEntity(
+    //
+    // A API da Marvel retorna os dados em ordem alfabetica.
+    // O Room ordena automaticamente pelo id.
+    // Precisamos do atributo autoId para não termos problema com a ordenação dos dados.
+    // Com o autoId nao perdemos a ordem alfabetica dos dados retornados pela API.
+    //
     @PrimaryKey(autoGenerate = true)
     val autoId: Int = 0,
     @ColumnInfo(name = DbConstants.CHARACTERS_COLUMN_INFO_ID)
