498bceab703fd0942029972f91f8013cca66bfe4
Vinicius Janner <viniciusjanner@gmail.com>
2023-08-26 19:04:29 -0300

    Uncomment instrumented test in CharactersFragmentTest.kt

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -4,14 +4,17 @@ import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.marvelapp.R
 import com.example.marvelapp.extension.asJsonString
 import com.example.marvelapp.framework.di.BaseUrlModule
 import com.example.marvelapp.framework.di.CoroutinesModule
 import com.example.marvelapp.launchFragmentInHiltContainer
+import com.example.marvelapp.presentation.characters.adapters.CharactersViewHolder
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
@@ -63,47 +66,47 @@ class CharactersFragmentTest {
         // Assert
     }
 
-//    @Test
-//    fun shouldLoadMoreCharacters_whenNewPageIsRequested(): Unit = runBlocking {
-//        //
-//        // deve carregar mais Characters quando uma nova página for solicitada
-//        //
-//
-//        // Arrange
-//        with(server) {
-//            enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
-//            enqueue(MockResponse().setBody("characters_p2.json".asJsonString()))
-//        }
-//
-//        delay(700) // delay porque utilizamos cache.
-//
-//        // Action
-//        onView(
-//            withId(R.id.recyclerCharacters),
-//        ).perform(
-//            RecyclerViewActions.scrollToPosition<CharactersViewHolder>(20),
-//        )
-//
-//        // Assert
-//        onView(withText("Amora")).check(matches(isDisplayed()))
-//    }
+    @Test
+    fun shouldLoadMoreCharacters_whenNewPageIsRequested(): Unit = runBlocking {
+        //
+        // deve carregar mais Characters quando uma nova página for solicitada
+        //
 
-//    @Test
-//    fun shouldShowErrorView_whenReceivesAnErrorFromApi(): Unit = runBlocking {
-//        //
-//        // deve mostrar visualização de erro quando recebe um erro da API
-//        //
-//
-//        // Arrange
-//        server.enqueue(MockResponse().setResponseCode(404))
-//
-//        delay(700) // delay porque utilizamos cache.
-//
-//        // Action
-//        onView(withId(R.id.textInitialLoadingError)).check(matches(isDisplayed()))
-//
-//        // Assert
-//    }
+        // Arrange
+        with(server) {
+            enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
+            enqueue(MockResponse().setBody("characters_p2.json".asJsonString()))
+        }
+
+        delay(700) // delay porque utilizamos cache.
+
+        // Action
+        onView(
+            withId(R.id.recyclerCharacters),
+        ).perform(
+            RecyclerViewActions.scrollToPosition<CharactersViewHolder>(20),
+        )
+
+        // Assert
+        onView(withText("Amora")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun shouldShowErrorView_whenReceivesAnErrorFromApi(): Unit = runBlocking {
+        //
+        // deve mostrar visualização de erro quando recebe um erro da API
+        //
+
+        // Arrange
+        server.enqueue(MockResponse().setResponseCode(404))
+
+        delay(700) // delay porque utilizamos cache.
+
+        // Action
+        onView(withId(R.id.textInitialLoadingError)).check(matches(isDisplayed()))
+
+        // Assert
+    }
 
     @After
     fun tearDown() {
